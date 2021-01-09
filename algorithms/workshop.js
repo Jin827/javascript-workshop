25c25605f6beaf3a85f76ee666efb8b0c999e82b
Jin827 <25231717+Jin827@users.noreply.github.com>
2021-01-09 12:44:36 +0900

    fibonacci sequence

--- algorithms/workshop.js
+++ algorithms/workshop.js
@@ -1,1 +1,17 @@
-console.log('hello')
+/**
+ * Q) return the nth element of the Fibonacci sequence.
+ * [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,...]
+ */ 
+
+function fibonacci(n) {
+    let group = [1, 1];
+
+    for (let i = 0; i < n - 2; i++) {
+        const value = group[i] + group[i +  1];
+        group.push(value);
+    };
+
+    return group;
+}
+
+console.log("fibonacci", fibonacci(10));
