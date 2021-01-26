fe60a7bd429dc8a0e49694d5fed245e0f211cf53
Jin827 <25231717+Jin827@users.noreply.github.com>
2021-01-26 11:26:17 +0900

    cartesian product algorithm without limits

--- algorithms/sets.js
+++ algorithms/sets.js
@@ -1,9 +1,14 @@
+/* Cartesian Product */
 function cartProduct(setA, setB) {
     const product = [];
 
-    setA.forEach((color) => {
-        for (const size of setB) {
-            product.push([color, size]);
+    setA.forEach((setAEl) => {
+        if (!Array.isArray(setAEl)) {
+            setAEl = [setAEl];
+        }
+
+        for (const setBEl of setB) {
+            product.push([...setAEl, setBEl]);
         }
     });
 
@@ -11,7 +16,22 @@ function cartProduct(setA, setB) {
 }
 
 const colors = ['blue', 'red'];
-const size = ['m', 'l'];
-console.log('cartProduct', cartProduct(colors, size));  // [['blue','m], ...]
+const size = ['s', 'm', 'l', 'xl'];
+const styles = ['round neck', 'v neck'];
+// console.log('cartProduct', cartProduct(colors, size));  // [['blue','m], ...]
 // Time Complexity: O(n*m) => O(n^2)
 // Space Complexity: O(n*m) => O(n^2) - create one new value[color,size] in product array on every loop.
+
+/* Cartesian Product without Limits */
+function cartesian(...sets) {
+    let product = sets[0];
+
+    for (let i = 1; i < sets.length; i++) {
+        product = cartProduct(product, sets[i]);
+    }
+
+    return product;
+}
+console.log('cartesian', cartesian(colors, size, styles));  // [['blue','m], ...]
+// Time Complexity: O(n^x)
+// Space Complexity: O(n^x)
