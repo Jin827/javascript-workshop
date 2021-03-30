bb940bd62c2c0dbc841f8c201cceb053e453f3b5
Jin827 <25231717+Jin827@users.noreply.github.com>
2021-03-30 22:10:15 +0900

    프로그래머스 algorithms

---
+++ algorithms/progrmmers/202103.js
@@ -0,0 +1,76 @@
+// 두개 뽑아서 더하기
+function solution(numbers) {
+    const answer = [];
+
+    for (let outer = 0; outer <= numbers.length - 2; outer++) {
+        for (let inner = outer + 1; inner <= numbers.length - 1; inner++) {
+            const sum = numbers[outer] + numbers[inner];
+
+            if (!answer.includes(sum)) {
+                answer.push(sum);
+            }
+        }
+    }
+    return answer.sort((a, b) => a - b);
+}
+// console.log('solution', solution([5, 0, 2, 7]));
+
+// 인형뽑기
+function solution(board, moves) {
+    const copied = [...board];
+    const store = []; // 바구니
+    let count = 0; // 제거된 인형의 개수
+
+    for (const move of moves) {
+        for (let i = 0; i < copied.length; i++) {
+            const value = copied[i][move - 1];
+
+            if (value !== 0) {
+                if (value === store[store.length - 1]) {
+                    // 같은 인형인 경우, 두 인형 제거
+                    store.pop();
+                    count += 2;
+                } else {
+                    // 뽑은 인형 바구니에 저장
+                    store.push(value);
+                }
+                // 뽑은 인형을 인형뽑기 기계에서 제거
+                copied[i][move - 1] = 0;
+                break;
+            }
+        }
+    }
+    return count;
+}
+const board = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]];
+const moves = [1, 5, 3, 5, 1, 2, 1, 4];
+// console.log('solution', solution(board, moves));
+
+// 완주하지 못한 선수
+function ascending(a, b) {
+    if (a < b) {
+        return -1;
+    }
+    if (a > b) {
+        return 1;
+    }
+    return 0;
+}
+
+function solution(participant, completion) {
+    let incompleted = '';
+    const sortedParticipant = participant.sort(ascending);
+    const sortedCompletion = completion.sort(ascending);
+
+    for (let i = 0; i < sortedParticipant.length; i++) {
+        // 두 그룹의 이름이 일치하지 않거나 or 모두 일치하고 참자가 그룹에만 한명이 남은 경우
+        if ((sortedParticipant[i] !== sortedCompletion[i]) || (i === sortedParticipant.length - 1)) {
+            incompleted = sortedParticipant[i];
+            break;
+        }
+    }
+    return incompleted;
+}
+const participant = ["marina", "josipa", "nikola", "vinko", "filipa"];
+const completion = ["josipa", "filipa", "marina", "nikola"];
+// console.log('solution', solution(participant, completion));
--- basic/variable.js
+++ basic/variable.js
@@ -1,30 +1,66 @@
 console.log("2 + 3 = " + add1(2, 3)); // “2 + 3 = 5”
-console.log("4 + 5 = " + add2(4, 5)); // Uncaught TypeError: add2 is not a function
-
+// console.log("4 + 5 = " + add2(4, 5)); // Uncaught TypeError: add2 is not a function
 function add1(a, b) { return a + b; }
 var add2 = function (a, b) { return a + b; };
 
-console.log('x', x); // referenceError : x is not defined
-var x = 4;
-var fn = function () {
-    console.log('x', x); // undefined, if remove ‘var x=2’ then x will be 4 !!
-    console.log('z', z); // referenceError : z is not defined (block scope)
-    console.log('y', y); // 2 (variable hoisting)
 
-    var x = 2;
+let x; // 이는 let x = undefined; 와 같다.
+// const 키워드는 반드시 선언과 동시에 값을 할당해야 한다.
+const x; // Uncaught SyntaxError: Missing initializer in const declaration;
+
+console.log('x', x); // undefined
+var x = 'outer scope';
+
+(function () {
+    // 스코프 안에서 선언된 변수는 항상 최상위에 선언한 것과 동등한 의미를 가진다.
+    console.log(x); // undefined
+    var x = 'inner scope';
+}());
+
+// 위의 코드는 아래와 같은 의미를 지닌다.
+var x = 'outer scope';
+(function () {
+    var x;
+    console.log(x); // undefined
+    x = 'inner scope';
+}());
+console.log('x', x); // outer scope
+
+if (true) {
+    var x = 'if condition scope';
+}
+console.log('x', x); // if condition scope
+
+
+(function outerFn() {
+    // console.log('z', z); // referenceError : z is not defined (block scope)
     let z = 3;
-    var y; // (variable declaration)
 
-    y = 2;
+    console.log('y', y); // referenceError : y is not defined 
+    var y; // y 선언
+    console.log('y', y); // undefined
+    y = 2; // y 할당
+    console.log('y', y); // 2
 
-    console.log('innerFn', innerFn); // 2 (function declaration hoisting)
-
+    console.log('innerFn', innerFn()); // 2 (function declaration hoisting)
     function innerFn() {
         return 2;
     }
-};
-console.log('fn', fn);
-{ let name = 'seo'; }
-console.log('name', name); // referenceError : name is not defined
+})();
+
+
+
 { var age = 29; }
 console.log('age', age); // 29, var은 function level scope
+
+{ let name = 'seo'; }
+console.log('name', name); // referenceError : name is not defined
+
+const c = 'hihi';
+{
+    const c = 'yahoo';
+    console.log('c', c); // yahoo
+}
+console.log('c', c); // hihi
+
+
